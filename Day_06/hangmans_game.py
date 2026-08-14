152cb6f887f90fcdde5f3278f13e4bd96087ef45
HubertShimiza <hudson.shimiza@gmail.com>
2026-08-14 16:10:48 -0300

    Update hangmans_game.py

--- Day_06/hangmans_game.py
+++ Day_06/hangmans_game.py
@@ -2,33 +2,32 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
+display = []
+life = 6
 for _ in range(len(chosen_word)):
     display.append("_")
-life = 6
+
 while life > 0:
     guess = input("Guess a letter: ").lower()
-    length = len(chosen_word)
-    tam_word = ""
-
-    for i in range(length):
-        tam_word += "_"
-        display = ""
-
-        for letter in chosen_word:
+    
+    # Se a letra escolhida estiver na palavra, atualiza o display
+    if guess in chosen_word:
+        for position in range(len(chosen_word)):
+            letter = chosen_word[position]
             if letter == guess:
-                display += letter
-            else:
-                display += "_"
-                life -= 1
+                display[position] = letter
+    # Se não estiver na palavra, perde apenas UMA vida
+    else:
+        life -= 1
+        
+    print(f"Word: {' '.join(display)}")
+    print(f"Lifes: {life}")
 
-        if "_" not in display:
-            print("You win!")
-            break
-print(tam_word)
-
-print (length)
-
-
-
-
-print(display)
+    # Checa se o jogador venceu
+    if "_" not in display:
+        print("You win!")
+        break
+        
+# Se sair do while e o jogador não tiver ganho, significa que a vida zerou
+if life == 0:
+    print("You lose!")
